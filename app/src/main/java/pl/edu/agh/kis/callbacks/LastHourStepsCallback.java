264124ff3bc63ec3d3bd8c515b675a61b736b3ea
Ewa Stachow <ewa.stachow@sabre.com>
2020-02-06 01:17:51 +0100

    Add LastHourSteps

---
+++ app/src/main/java/pl/edu/agh/kis/callbacks/LastHourStepsCallback.java
@@ -0,0 +1,42 @@
+package pl.edu.agh.kis.callbacks;
+
+import android.util.Log;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import heart.Callback;
+import heart.WorkingMemory;
+import heart.alsvfd.SimpleNumeric;
+import heart.exceptions.AttributeNotRegisteredException;
+import heart.exceptions.NotInTheDomainException;
+import heart.xtt.Attribute;
+import pl.edu.agh.kis.StatisticKeeper;
+
+public class LastHourStepsCallback implements Callback {
+
+    private final String TAG = getClass().getSimpleName();
+
+    @Override
+    public void execute(Attribute attribute, WorkingMemory workingMemory) {
+        Log.i(TAG, "Executing LastHourStepsCallback for " + attribute.getName());
+        try {
+            workingMemory.setAttributeValue(attribute, new SimpleNumeric(resolveLastHourSteps()), false);
+        } catch (AttributeNotRegisteredException | NotInTheDomainException e) {
+            Log.i(TAG, "Error occurred: ", e);
+        }
+    }
+
+    private double resolveLastHourSteps() {
+        List<Map.Entry<LocalDateTime, Float>> todays = StatisticKeeper.getStepsData().entrySet().stream()
+                .filter(entry -> entry.getKey().isAfter(LocalDateTime.now().minusHours(1)))
+                .sorted((ldt1, ldt2) -> ldt1.getKey().compareTo(ldt2.getKey()))
+                .collect(Collectors.toList());
+        double end = todays.get(todays.size() - 1).getValue().doubleValue();
+        double begin = todays.get(0).getValue().doubleValue();
+        return end - begin;
+    }
+
+}
