cb8d168f8756c792b71f2fb366585ad9cd70283a
Ewa Stachow <ewa.stachow@sabre.com>
2020-02-06 02:02:36 +0100

    Implement inaction in minutes

--- app/src/main/java/pl/edu/agh/kis/callbacks/InactionTimeInMinutesCallback.java
+++ app/src/main/java/pl/edu/agh/kis/callbacks/InactionTimeInMinutesCallback.java
@@ -2,12 +2,19 @@ package pl.edu.agh.kis.callbacks;
 
 import android.util.Log;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import heart.Callback;
 import heart.WorkingMemory;
 import heart.alsvfd.SimpleNumeric;
 import heart.exceptions.AttributeNotRegisteredException;
 import heart.exceptions.NotInTheDomainException;
 import heart.xtt.Attribute;
+import pl.edu.agh.kis.StatisticKeeper;
 
 public class InactionTimeInMinutesCallback implements Callback {
 
@@ -24,8 +31,32 @@ public class InactionTimeInMinutesCallback implements Callback {
     }
 
     private double resolveInactionTimeInMinutes() {
-        // TODO Implement
-        return 10;
+        List<Map.Entry<LocalDateTime, Float>> collect = StatisticKeeper.getStepsData().entrySet().stream()
+                .sorted((ldt1, ldt2) -> ldt2.getKey().compareTo(ldt1.getKey()))
+                .collect(Collectors.toList());
+        LocalDateTime newestTime = collect.get(0).getKey();
+        Float newestSteps = collect.get(0).getValue();
+        Duration inaction = Duration.ofHours(24);
+        for (Map.Entry<LocalDateTime, Float> entry: collect) {
+            LocalDateTime time = entry.getKey();
+            Float steps = entry.getValue();
+            boolean isStillInactive = isStillInactive(newestTime, time, newestSteps, steps);
+            if (isStillInactive) {
+                inaction = Duration.between(time, newestTime);
+            } else if (newestTime != time) {
+                return inaction.toMinutes();
+            }
+        }
+        return inaction.toMinutes();
+    }
+
+    private boolean isStillInactive(LocalDateTime newestTime, LocalDateTime time, Float newestSteps, Float steps) {
+        if (newestTime == time) {
+            return true;
+        }
+        double duration = Duration.between(time, newestTime).toMinutes();
+        float stepsDiff = newestSteps - steps;
+        return stepsDiff < 20 || stepsDiff / duration < 30;
     }
     
 }
